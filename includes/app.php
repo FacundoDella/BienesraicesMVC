69c394840d942610cb83d6c3ebfb9486d8dd3c31
Facundo Dellanegra <facundodiciembre25@gmail.com>
2024-04-23 19:30:41 -0300

    Cambios en app.php

--- includes/app.php
+++ includes/app.php
@@ -1,10 +1,9 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-use Dotenv\Dotenv;
 use Model\ActiveRecord; // 3 Mandamos a llamar a la class ActiveRecord
+require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeload();
 
 require 'funciones.php';
